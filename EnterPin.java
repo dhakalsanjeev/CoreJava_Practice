7f0290a2b81608b954dcc92164f9da44fe717cb7
Sanjeev Dhakal <islington.sanjeev@gmail.com>
2017-03-08 09:27:27 +1100

    Add files via upload

--- EnterPin.java
+++ EnterPin.java
@@ -3,11 +3,25 @@ import java.util.Scanner;
 public class EnterPin {
 	public static void main ( String[] args ) {
 		Scanner keyboard = new Scanner(System.in); 
+		String password, enter; 
 		int pin, entry; 
+		
+		password = "Sanjeev";
 		
 		pin = 12345; 
 		
-		System.out.println("WELCOME TO THE BANK OF jAVA"); 
+		System.out.println("WELCOME TO THE BANK OF jAVA");
+		System.out.println("PLEASE ENTER THE PASSWORD");
+		enter = keyboard.nextLine();
+		
+			while (! enter.equals("Sanjeev")) {
+				System.out.println("\nINCORRECT PASSWOORD, TRY AGAIN");
+				System.out.print("ENTER THE PASSWORD ");
+				enter = keyboard.nextLine();
+			}
+			
+			System.out.println("\n PASSWORD MATCHED, NOW PLEASE ENTER YOUR PIN");
+		
 		System.out.print("ENTER YOUR PIN: "); 
 		entry = keyboard.nextInt(); 
 		
